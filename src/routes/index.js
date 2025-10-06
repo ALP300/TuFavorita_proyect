9445c007174123a7a79ded1b8ad01dda765644e2
ALP300 <aitorpalomino1@gmail.com>
2025-10-05 20:41:46 -0500

    a

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,5 +8,11 @@ router.get("/", (req, res)=>{
 router.get("/catalogo", (req, res)=>{
     res.render("catalogo",{title: "Catálogo"})
 });
+router.get("/promociones", (req, res)=>{
+    res.render("promociones",{title: "Tus promos"})
+});
+router.get("/catalogo", (req, res)=>{
+    res.render("catalogo",{title: "Catálogo"})
+});
 
 export {router};
